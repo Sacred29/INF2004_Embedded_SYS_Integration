1b8d5d0ab05c8437b65dbacc3229fa4a073e8225
“Hanwei43” <tohhanwei9645@gmail.com>
2024-10-30 18:23:49 +0800

    updated the ultrasonic driver

--- ultrasonic_driver/ultrasonic_driver.c
+++ ultrasonic_driver/ultrasonic_driver.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+// Get readings from ultrasonic sensor
 
 #include "pico/stdlib.h"
+#include <stdio.h>
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 
 int timeout = 26100;
+
+absolute_time_t startTime;
+absolute_time_t endTime;
 
 void setupUltrasonicPins(uint trigPin, uint echoPin)
 {
@@ -12,6 +16,37 @@ void setupUltrasonicPins(uint trigPin, uint echoPin)
      gpio_init(echoPin);
      gpio_set_dir(trigPin, GPIO_OUT);
      gpio_set_dir(echoPin, GPIO_IN);
+}
+
+void sentTrigPulse(uint trigPin, uint echoPin)
+{
+     gpio_put(trigPin, 1);
+     sleep_us(10);
+     gpio_put(trigPin, 0);
+}
+
+uint64_t measureEchoPulse(uint trigPin, uint echoPin)
+{
+     uint64_t width = 0;
+     startTime = get_absolute_time();
+
+     while (gpio_get(echoPin) == 1)
+     {
+          endTime = get_absolute_time();
+          if (absolute_time_diff_us(startTime, endTime) > timeout)
+               return 0;
+     }
+
+     return absolute_time_diff_us(startTime, endTime);
+}
+
+double getDistance(uint trigPin, uint echoPin)
+{
+     uint64_t pulseLength = measureEchoPulse(trigPin, echoPin);
+     printf("IR %llu us, ", pulseLength);
+     double distance = (pulseLength / 29) * 0.5; // Calculate with higher precision
+     printf("IR %f cm\n", distance);
+     return distance;
 }
 
 uint64_t getPulse(uint trigPin, uint echoPin)
@@ -24,7 +59,7 @@ uint64_t getPulse(uint trigPin, uint echoPin)
 
      while (gpio_get(echoPin) == 0)
           tight_loop_contents();
-     absolute_time_t startTime = get_absolute_time();
+     absolute_time_t tempStartTime = get_absolute_time();
      while (gpio_get(echoPin) == 1)
      {
           width++;
@@ -32,19 +67,16 @@ uint64_t getPulse(uint trigPin, uint echoPin)
           if (width > timeout)
                return 0;
      }
-     absolute_time_t endTime = get_absolute_time();
+     absolute_time_t tempEndTime = get_absolute_time();
 
-     return absolute_time_diff_us(startTime, endTime);
+     return absolute_time_diff_us(tempStartTime, tempEndTime);
 }
 
-uint64_t getCm(uint trigPin, uint echoPin)
+double getCm(uint trigPin, uint echoPin)
 {
      uint64_t pulseLength = getPulse(trigPin, echoPin);
-     return pulseLength / 29 / 2;
-}
-
-uint64_t getInch(uint trigPin, uint echoPin)
-{
-     uint64_t pulseLength = getPulse(trigPin, echoPin);
-     return (long)pulseLength / 74.f / 2.f;
+     printf("%llu us, ", pulseLength);
+     double distance = pulseLength / 29 / 2; // Calculate with higher precision
+     printf("%f cm\n", distance);
+     return distance;
 }
--- ultrasonic_driver/ultrasonic_driver.h
+++ ultrasonic_driver/ultrasonic_driver.h
@@ -1,6 +1,9 @@
 #ifndef ultrasonic_driver_h
 #define ultrasonic_driver_h
-void setupUltrasonicPins(int trigPin, int echoPin);
-int getCm(int trigPin, int echoPin);
-int getInch(int trigPin, int echoPin);
+void setupUltrasonicPins(uint trigPin, uint echoPin);
+void sentTrigPulse(uint trigPin, uint echoPin);
+uint64_t measureEchoPulse(uint trigPin, uint echoPin);
+double getDistance(uint trigPin, uint echoPin);
+uint64_t getPulse(uint trigPin, uint echoPin);
+double getCm(uint trigPin, uint echoPin);
 #endif
