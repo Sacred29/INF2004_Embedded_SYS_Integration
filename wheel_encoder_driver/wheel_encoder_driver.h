d3b40beea09d24babe5112ce3dcefcf7e189f163
ngjocelyn <61549522+ngjocelyn@users.noreply.github.com>
2024-11-02 09:25:53 +0800

    added to reset distance to zero

--- wheel_encoder_driver/wheel_encoder_driver.h
+++ wheel_encoder_driver/wheel_encoder_driver.h
@@ -3,6 +3,8 @@
 void setupWheelEncoderPins(uint left_wheel_pin, uint right_wheel_pin);
 void leftWheelPulseCounting();
 void rightWheelPulseCounting();
-float getLeftWheelSpeed(float pulse_width);
-float getRightWheelSpeed(float pulse_width);
+void getLeftWheelInfo(float *left_wheel_speed, float *left_distance_travel);
+void getRightWheelInfo(float *right_wheel_speed, float *right_distance_travel);
+void calculateTargetTicks(float wheel_diameter, float wheelbase, int pulses_per_revolution);
+void resetDistanceTraveled(float *left_distance, float *right_distance);
 #endif
